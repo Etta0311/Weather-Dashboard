1e875c4159c6694f3db4b3186f5495bafb3490a3
ettaho <etta0311031@gmail.com>
2021-09-30 23:12:56 +1000

    update

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # wk6-Weather-Dashboard
+Link: [weather Dashboard](https://etta0311.github.io/wk6-Day-Planner/)
+
+## Description
+
+This work is to build a weather Dashboard from scrach which it give data of current weather and 5 day weather forecast of the city that typed in for searching.
+This is the website are using an API called openweathermap.
+website not only showing data when it received a user input but also called up the previous search by clicking in corresponding part.
+
+## Table of Contents
+
+   - Techniques used
+   - Usage
+   - Credits
+   - License
+
+## Techniques used
+
+   - HTML
+   - CSS
+   - Javascript
+   - jQuery 
+   - Bootstrap
+   - 
+
+
+## Usage
+![Demo](./RMimages/demo.gif)
+
+## Contributors
+
+![contribution](./RMimages/contributors.png)
+
+## Credits
+    - https://coding-boot-camp.github.io/full-stack/github/professional-readme-guid
+    - https://choosealicense.com/
+    - https://jquery.com/
+    - https://getbootstrap.com/docs/5.1/getting-started/introduction/
+    - https://openweathermap.org/api
+    - https://rapidapi.com/blog/access-global-weather-data-with-these-weather-apis/
+    - https://fonts.google.com/
+    - https://htmlcolorcodes.com/
+    - https://cssgradient.io/
+    - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+    - https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+    - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+    - https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+    - https://javascript.info/promise-api
+    - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    - https://developer.mozilla.org/en-US/docs/Web/API/fetch
+    
+## License
+MIT License
+
+Copyright (c) [2021] [Ettaho]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+=======
--- script.js
+++ script.js
@@ -9,144 +9,125 @@ var Currenthumidity = $(".Currenthumidity");
 var CurrentFLtemp = $(".CurrentFLtemp");
 var Currenticon = $(".Currenticon");
 var searchHistory = [];
+var historyBtn = $(".historybox");
+var cityHistory;
 
 searchBtn.on("click", search);
+historyBtn.on("click", search);
 
 function search() {
-    // get value of cityname input by user
-    var cityinput = $(".cityinput").val();
-    console.log(cityinput);
 
-    // save search history to localstorage and display in previous search part
-    if (searchHistory.indexOf(cityinput) !== -1) {
-        return;
-      }
-    searchHistory.push(cityinput);
-    localStorage.setItem('history', JSON.stringify(searchHistory));
+   // get value of cityname input by user
+   var cityinput = $(".cityinput").val();
+   console.log(cityinput);
 
-    var cityHistory = JSON.parse(localStorage.getItem('history'));
-    document.body.querySelector("ul").innerHTML = "";
+   // save search history to localstorage and display in previous search part
+   if (searchHistory.indexOf(cityinput) !== -1) {
+       return;
+     }
+   searchHistory.push(cityinput);
+   localStorage.setItem('history', JSON.stringify(searchHistory));
 
-    for (x = 0; x < cityHistory.length; x++) {
-        var historyBtn = document.createElement("button");
-        historyBtn.classList.add("row");
-        historyBtn.classList.add("btn-secondary");
-        historyBtn.textContent = cityHistory[x];
-        document.body.querySelector("ul").appendChild(historyBtn)
-        historyBtn.addEventListener("click", function(event){buttonSearch(event.target.innerHTML)});
-    }
+   cityHistory = JSON.parse(localStorage.getItem('history'));
+   document.body.querySelector("ul").innerHTML = "";
 
-    //Get current weather data from API
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityinput + '&appid=' + APIkey + "&units=metric")
-    .then(response => response.json())
-    .then(data => {
-        var cityname = data.name;
-        var countryname = data.sys.country;
-        var tempinC = data.main.temp;
-        var humidity = data.main.humidity;
-        var FLtemp = data.main.feels_like;
-        var icon = data.weather[0].icon;
-        var iconlink = "http://openweathermap.org/img/w/" + icon + ".png";
+   for (x = 0; x < cityHistory.length; x++) {
+       var historyBtn = document.createElement("button");
+       historyBtn.classList.add("row");
+       historyBtn.classList.add("btn-secondary");
+       historyBtn.classList.add("historyBtn");
+       historyBtn.textContent = cityHistory[x];
+       document.body.querySelector("ul").appendChild(historyBtn)
 
-        Currentcityname.text(cityname + " , " + countryname);
-        CurrenttempinC.text("Current temperature : " + tempinC + "°C");
-        Currenthumidity.text("Humidity : " + humidity + " %");
-        CurrentFLtemp.text("Feels like : " + FLtemp + "°C");
-        Currenticon.attr('src', iconlink);
+   }
 
-    fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + cityinput + '&appid=' + APIkey + "&units=metric")
-    .then(secresponse => secresponse.json())
-    .then(secdata => {
-        var forcastdate = [secdata.list[0], secdata.list[8], secdata.list[16], secdata.list[24], secdata.list[32]];
-        $(".5daydata").empty()
+   //Get current weather data from API
+   fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityinput + '&appid=' + APIkey + "&units=metric")
+   .then(response => response.json())
+   .then(data => {
+       var cityname = data.name;
+       var countryname = data.sys.country;
+       var tempinC = data.main.temp;
+       var humidity = data.main.humidity;
+       var FLtemp = data.main.feels_like;
+       var icon = data.weather[0].icon;
+       var iconlink = "http://openweathermap.org/img/w/" + icon + ".png";
+       Currentcityname.text(cityname + " , " + countryname);
+       CurrenttempinC.text("Current temperature : " + tempinC + "°C");
+       Currenthumidity.text("Humidity : " + humidity + " %");
+       CurrentFLtemp.text("Feels like : " + FLtemp + "°C");
+       Currenticon.attr('src', iconlink);
 
-        forcastdate.forEach(function(i) {
-            var indicatedate = i.dt_txt;
-            var forcasticon = i.weather[0].icon;
-            var forcasticonlink = "http://openweathermap.org/img/wn/" + forcasticon + "@2x.png";
-            var forcasttemp = i.main.temp;
-            var forcasthumidity = i.main.humidity;
-            var forcastwind = i.wind.speed;
+   fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + cityinput + '&appid=' + APIkey + "&units=metric")
+   .then(secresponse => secresponse.json())
+   .then(secdata => {
+       var forcastdate = [secdata.list[0], secdata.list[8], secdata.list[16], secdata.list[24], secdata.list[32]];
+       $(".5daydata").empty()
 
-            $(".5daydata").append(
-                "<div class = col 5dayweather>" + 
-                "<p>" + indicatedate + "<br>" +
-                "<img src=" + forcasticonlink + ">"+ "<br>" +
-                "Temp : " + forcasttemp + "°C" + "<br>" +
-                "Humidity : " + forcasthumidity + "%" + "<br>" +
-                "Wind : " + forcastwind + " M/Sec" + 
-                "</p>")
-        })
+       forcastdate.forEach(function(i) {
+           var indicatedate = i.dt_txt;
+           var forcasticon = i.weather[0].icon;
+           var forcasticonlink = "http://openweathermap.org/img/wn/" + forcasticon + "@2x.png";
+           var forcasttemp = i.main.temp;
+           var forcasthumidity = i.main.humidity;
+           var forcastwind = i.wind.speed;
 
-
-    });
-    });
+           $(".5daydata").append(
+               "<div class = col 5dayweather>" +
+               "<p>" + indicatedate + "<br>" +
+               "<img src=" + forcasticonlink + ">"+ "<br>" +
+               "Temp : " + forcasttemp + "°C" + "<br>" +
+               "Humidity : " + forcasthumidity + "%" + "<br>" +
+               "Wind : " + forcastwind + " M/Sec" +
+               "</p>")
+       })
+   });
+   });
 }
 
-function buttonSearch(cityinput) {
+function buttonSearch(cityHistory) {
+    cityHistory = $(".historyBtn").text();
+    console.log(cityHistory);
 
-    // save search history to localstorage and display in previous search part
-    if (searchHistory.indexOf(cityinput) !== -1) {
-        return;
-      }
-    searchHistory.push(cityinput);
-    localStorage.setItem('history', JSON.stringify(searchHistory));
+   fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityHistory + '&appid=' + APIkey + "&units=metric")
+   .then(response => response.json())
+   .then(data => {
+       var cityname = data.name;
+       var countryname = data.sys.country;
+       var tempinC = data.main.temp;
+       var humidity = data.main.humidity;
+       var FLtemp = data.main.feels_like;
+       var icon = data.weather[0].icon;
+       var iconlink = "http://openweathermap.org/img/w/" + icon + ".png";
+       Currentcityname.text(cityname + " , " + countryname);
+       CurrenttempinC.text("Current temperature : " + tempinC + "°C");
+       Currenthumidity.text("Humidity : " + humidity + " %");
+       CurrentFLtemp.text("Feels like : " + FLtemp + "°C");
+       Currenticon.attr('src', iconlink);
 
-    var cityHistory = JSON.parse(localStorage.getItem('history'));
-    document.body.querySelector("ul").innerHTML = "";
-
-    for (x = 0; x < cityHistory.length; x++) {
-        var historyBtn = document.createElement("button");
-        historyBtn.classList.add("row");
-        historyBtn.classList.add("btn-secondary");
-        historyBtn.textContent = cityHistory[x];
-        document.body.querySelector("ul").appendChild(historyBtn)
-        historyBtn.addEventListener("click", secsearch());
-    }
-
-    //Get current weather data from API
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityinput + '&appid=' + APIkey + "&units=metric")
-    .then(response => response.json())
-    .then(data => {
-        var cityname = data.name;
-        var countryname = data.sys.country;
-        var tempinC = data.main.temp;
-        var humidity = data.main.humidity;
-        var FLtemp = data.main.feels_like;
-        var icon = data.weather[0].icon;
-        var iconlink = "http://openweathermap.org/img/w/" + icon + ".png";
-
-        Currentcityname.text(cityname + " , " + countryname);
-        CurrenttempinC.text("Current temperature : " + tempinC + "°C");
-        Currenthumidity.text("Humidity : " + humidity + " %");
-        CurrentFLtemp.text("Feels like : " + FLtemp + "°C");
-        Currenticon.attr('src', iconlink);
-
-    fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + cityinput + '&appid=' + APIkey + "&units=metric")
-    .then(secresponse => secresponse.json())
-    .then(secdata => {
-        var forcastdate = [secdata.list[0], secdata.list[8], secdata.list[16], secdata.list[24], secdata.list[32]];
-        $(".5daydata").empty()
-
-        forcastdate.forEach(function(i) {
-            var indicatedate = i.dt_txt;
-            var forcasticon = i.weather[0].icon;
-            var forcasticonlink = "http://openweathermap.org/img/wn/" + forcasticon + "@2x.png";
-            var forcasttemp = i.main.temp;
-            var forcasthumidity = i.main.humidity;
-            var forcastwind = i.wind.speed;
-
-            $(".5daydata").append(
-                "<div class = col 5dayweather>" + 
-                "<p>" + indicatedate + "<br>" +
-                "<img src=" + forcasticonlink + ">"+ "<br>" +
-                "Temp : " + forcasttemp + "°C" + "<br>" +
-                "Humidity : " + forcasthumidity + "%" + "<br>" +
-                "Wind : " + forcastwind + " M/Sec" + 
-                "</p>")
-        })
-
-
-    });
-    })
+   fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + cityHistory + '&appid=' + APIkey + "&units=metric")
+   .then(secresponse => secresponse.json())
+   .then(secdata => {
+       var forcastdate = [secdata.list[0], secdata.list[8], secdata.list[16], secdata.list[24], secdata.list[32]];
+       $(".5daydata").empty()
+       forcastdate.forEach(function(i) {
+           var indicatedate = i.dt_txt;
+           var forcasticon = i.weather[0].icon;
+           var forcasticonlink = "http://openweathermap.org/img/wn/" + forcasticon + "@2x.png";
+           var forcasttemp = i.main.temp;
+           var forcasthumidity = i.main.humidity;
+           var forcastwind = i.wind.speed;
+           $(".5daydata").append(
+               "<div class = col 5dayweather>" +
+               "<p>" + indicatedate + "<br>" +
+               "<img src=" + forcasticonlink + ">"+ "<br>" +
+               "Temp : " + forcasttemp + "°C" + "<br>" +
+               "Humidity : " + forcasthumidity + "%" + "<br>" +
+               "Wind : " + forcastwind + " M/Sec" +
+               "</p>")
+       })
+   });
+   })
 }
+
+historyBtn.on('click', buttonSearch);
